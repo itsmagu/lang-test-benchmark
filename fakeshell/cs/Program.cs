d97ade0619cfdb68afa31e8766750cecd762aa79
magu <95685391+itsmagu@users.noreply.github.com>
2024-09-02 00:38:51 +0200

    started cs on fakeshell

--- fakeshell/cs/Program.cs
+++ fakeshell/cs/Program.cs
@@ -1,7 +1,41 @@
 ﻿using System;
+using static System.Console;
 
-public static class Program{
-	public static void Main(string[] args){
-		
-	}
+// I like the practice of passing methods like
+// I did in my Bombshell project, so let's recreate that.
+public static class Program
+{
+    public static void Main(string[] args){
+        while (!Shell.quitOnNext){
+            Write("\n$>");
+            Shell.RunCommand(ReadLine()!);
+        }
+    }
+}
+
+static class Shell
+{
+    internal static bool quitOnNext = false;
+    internal static void RunCommand(string command){ }
+}
+
+static class FileSystem
+{
+    static Folder root = new Folder(null, null);
+}
+
+class File
+{
+    public override string ToString() => "File";
+}
+
+class Folder
+{
+    public Folder(Folder[]? children, File[]? content){
+        this.children = children;
+        this.content = content;
+    }
+    public override string ToString() => "Folder";
+    public Folder[]? children;
+    public File[]? content;
 }

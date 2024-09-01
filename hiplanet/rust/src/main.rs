1f3cc650f0800c07fc75697d2818874ba36d2c75
magu <95685391+itsmagu@users.noreply.github.com>
2024-09-01 17:18:14 +0200

    zig and rust added to hiplanet

--- hiplanet/rust/src/main.rs
+++ hiplanet/rust/src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hi Planet");
+    let args : Vec<_> = env::args().collect();
+    if args.len() > 1{
+        println!("Also passed with {:?}",args[1]);
+    }
 }

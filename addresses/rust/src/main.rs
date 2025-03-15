21ecaa935f5a30b609d553b725da8f7483ba709f
magu <95685391+itsmagu@users.noreply.github.com>
2025-03-15 05:33:09 +0100

    Delete addresses/rust directory since It was never done

--- addresses/rust/src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-
-fn main() {
-    println!("Hi Planet");
-    let args : Vec<_> = env::args().collect();
-    if args.len() > 1{
-        println!("Also passed with {:?}",args[1]);
-    }
-}
